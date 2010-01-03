d824a7bf9885febe9fd47902bd19d297375ccc09
baxter <baxter@mog.ultima>
2010-01-03 23:26:30 +0000

    Commented out maruku - not supported by heroku

--- main.rb
+++ main.rb
@@ -1,16 +1,17 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
-require 'maruku'
+# require 'maruku'
 
 set :haml, {:format => :html5 }
 
 get '/*' do
   @title = "Welcome"
-  @content = Maruku.new("
-  Welcome
-  -------
-  Welcome to PaulBoxley.com
-  ").to_html
+  #@content = Maruku.new("
+  #Welcome
+  #-------
+  #Welcome to PaulBoxley.com
+  #").to_html
+  @content = "<h2>Welcome</h2><p>Welcome to PaulBoxley.com</p>"
   haml :index
 end
