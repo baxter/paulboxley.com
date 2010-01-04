c25febcca5ba30e9085d2210fb9aa36a421e97fc
baxter <baxter@mog.ultima>
2010-01-04 17:27:18 +0000

    Cleaned up main.rb and added Typekit support

--- main.rb
+++ main.rb
@@ -1,17 +1,11 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
-# require 'maruku'
 
 set :haml, {:format => :html5 }
 
 get '/*' do
   @title = "Welcome"
-  #@content = Maruku.new("
-  #Welcome
-  #-------
-  #Welcome to PaulBoxley.com
-  #").to_html
   @content = "<h2>Welcome</h2><p>Welcome to PaulBoxley.com</p>"
   haml :index
 end
