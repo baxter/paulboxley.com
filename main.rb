a2b06f9749993fb4f74dde8a6f77bdbfb6a610cd
baxter <baxter@Mog.local>
2010-01-31 21:41:10 +0000

    Replaced templates and added BlueCloth

--- main.rb
+++ main.rb
@@ -1,11 +1,28 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'bluecloth'
 
 set :haml, {:format => :html5 }
 
-get '/*' do
-  @title = "Welcome"
-  @content = "<h2>Welcome</h2><p>Welcome to PaulBoxley.com</p>"
-  haml :index
+def page ( page_name )
+  begin
+    @content = BlueCloth.new(File.read("pages/#{page_name}.md")).to_html
+  rescue
+    raise Sinatra::NotFound
+  end
+  @title = page_name
+  haml :default
+end
+
+get '/' do
+  page("index")
+end
+
+get '/:page' do
+  page(params[:page])
+end
+
+not_found do
+  page("404")
 end
---
+++ pages/index.md
@@ -0,0 +1,5 @@
+I am Paul Boxley, a software developer living in Edinburgh. This is my website! Awesome. Here is some markdown stuff:
+
+* This is a list item!
+* So is this.
+* I like markdown.
